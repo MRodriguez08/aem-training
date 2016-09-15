e5074862a9bde288d7e29c9f7c13b666a060d5fc
Mauricio Rodriguez <mau.rod.81090@gmail.com>
2016-09-15 11:25:00 -0300

    @Reference at class level definition

--- company-core/src/main/java/com/globant/aem/training/core/servlets/ScrTestServlet.java
+++ company-core/src/main/java/com/globant/aem/training/core/servlets/ScrTestServlet.java
@@ -8,12 +8,16 @@ import javax.servlet.ServletException;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.References;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+import com.day.commons.datasource.poolservice.DataSourcePool;
  
-@Component(metatype = true)
+@Component(metatype = true, immediate=false)
 @Service(value = Servlet.class)
 @Properties({
     @Property(name = "sling.servlet.paths", value = "/bin/training/slingservlettest3"),
@@ -22,7 +26,20 @@ import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
     @Property(name = "sling.servlet.extensions", value = "html"),
     @Property(name = "sling.servlet.methods", value = "GET")
 })
+@References({
+	@Reference( name = "dspService", referenceInterface = DataSourcePool.class, bind="bindDspService", unbind="unbindDspService")
+})
 public class ScrTestServlet extends SlingSafeMethodsServlet {
+	
+    private DataSourcePool dspService;
+
+	public void bindDspService(DataSourcePool dspService) {
+		this.dspService = dspService;
+	}
+	
+	public void unbindDspService(DataSourcePool dspService) {
+		this.dspService = dspService;
+	}
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
